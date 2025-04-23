7ab8065467078eacdc3da744c90a774c53d12e7c
zxx <1958726832@qq.com>
2025-04-23 22:29:30 +0800

    复习一部分

---
+++ test_4_23_review/test.c
@@ -0,0 +1,227 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+
+
+//#include <stdint.h>
+
+
+//typedef struct node
+//{
+//	int n;
+//	struct node* next;
+//}node;
+//
+//int main()
+//{
+//	//NULL;
+//	//null;
+//	node node2 = { 2,NULL };
+//	node node1 = { 1,&node2 };
+//	printf("%d\n", (node1.next)->n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) == 1)
+//	{
+//		;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+//extern int add(int x, int y);
+//int main()
+//{
+//	//printf("%d\n", sizeof(int));
+//	//printf("%d\n", sizeof(long));
+//	//printf("%d\n", sizeof(long long));
+//	printf("%d\n", add(1, 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%d\n", '\17');
+//	//printf("%d\n", '\x17');
+//
+//	//int a = 1;
+//	//int b = 2;
+//	//if (a == 1 || b == 2)
+//	//{
+//	//	printf("1");
+//	//}
+//
+//	//if (0)
+//	//{
+//	//	printf("1");
+//	//}
+//
+//	int a = 1;
+//	int b = 2;
+//	//int max = (a > b ? a : b);
+//	int max = 0;
+//	if (a > b)
+//		max = a;
+//	else
+//		max = b;
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+//typedef unsigned int uint;
+//
+//int main()
+//{
+//	//int a = 1;
+//	//int b = 2;
+//	//int x = ((a += 1), a + 1);
+//	//printf("%d\n", x);
+//	uint x = -1;
+//	printf("%u\n", x);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%d\n", (!0));
+//	int age = 23;
+//	if (22 <= age && age <= 30)
+//		printf("good");
+//	return 0;
+//}
+//EOF;
+
+//int main()
+//{
+//	//char ch = 0;
+//	//while ((ch = getchar()) != '\n')
+//	//{
+//	//	putchar(ch);
+//	//}
+//
+//	char ch = getchar();
+//	putchar(ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[20] = "xxxxxxxxxxxxxxxxxxx"; char ch = 0;
+//	scanf("%s", arr);
+//	//scanf("%c", &ch);
+//	ch = getchar();
+//	printf("%s\n", arr);
+//	printf("%d\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//char ch = 0; char tmp = 0;
+//	//scanf("%ca%c", &ch,&tmp);
+//	//printf("%c\n", ch);
+//	//printf("%c\n", tmp);
+//	intmax_t; size_t;
+//	printf("%d\n", sizeof(long double));
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	
+//	int ret = 1;
+//
+//
+//	for (int i = 0, ret = 0; i < 3; i++, ret++)
+//	{
+//		;
+//	}
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	do
+//	{
+//		;
+//	} while(1);
+//	return 0;
+//}
+
+//#include <windows.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	printf("hello world\n");
+//	Sleep(10000);
+//	printf("Çå¿ÕÆÁÄ»\n");
+//	Sleep(1000);
+//	system("cls");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("%2d\n", 1);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	int num = 5;
+//	while (num--)
+//	{
+//		printf("%d\n", rand()%10+1);
+//
+//	}
+//	srand(1);
+//	num = 5;
+//	while (num--)
+//	{
+//		printf("%d\n", rand() % 10 + 1);
+//
+//	}
+//	//printf("num = %d\n", num);
+//	return 0;
+//}
+//typedef unsigned int uint;
+//int main()
+//{
+//	srand((uint)time(NULL));
+//	int num = 5;
+//	while (num--)
+//	{
+//		printf("%d\n", rand() % 10 + 1);
+//	}
+//	srand(1); num = 5;
+//	while (num--)
+//	{
+//		printf("%d\n", rand() % 10 + 1);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//begin:
+//	printf("hehe\n");
+//	goto begin;
+//	return 0;
+//}
+
