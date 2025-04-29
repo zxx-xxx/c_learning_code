3d0e67889f25ba6f771eeadd6f7725d623f5c04f
zxx <1958726832@qq.com>
2025-04-29 23:04:47 +0800

    Create test.c

---
+++ test_4_29/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//int main()
+//{
+//	//null;
+//	//printf("%c\n", '\a');
+//	//printf("hello world\b");
+//	//printf("hello world\b");
+//	//while (1)
+//	//{
+//	//}
+//
+//	//char arr[10] = "*******";
+//	//scanf("%s", arr);
+//	//char ch = getchar();
+//
+//	//char arr[50] = "abc"
+//	//	"def";
+//	
+//	//printf("a\tb\nc\td");
+//
+//	//printf("hello\rworld");
+//	//system("cls");
+//
+//	//printf("hello world\r");
+//	//while (1);
+//
+//	//printf("\vhello world");
+//
+//	//printf("Name:\tAlice\nAge:\t30\n");
+//
+//	//printf("\fhello world");
+//	//printf("\fhello world\n");
+//
+//	return 0;
+//}
